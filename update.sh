8874da5d68459fc0fa78e9d2cbbb1f46bd09c679
zxymark221 <zxymark@vt.edu>
2016-01-30 17:07:28 -0500

    Collaborator push test

--- update.sh
+++ update.sh
@@ -1,1 +1,5 @@
 echo 'I am in update branch!'
+
+echo 'zxymark221 updates this code as a collaborator'
+
+echo 'As soon as we push this commit to the repo, I think we had a good understanding towards GitHub now and we will start our development in the future.'
